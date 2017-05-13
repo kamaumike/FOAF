1146ab182611a6d6ee655d206911e88dbc4bb718
kamaumike <kamaumike2013@gmail.com>
2017-05-13 22:15:56 +0300

    Create navigation menu

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,43 @@
+<!-- Start  Logo & Naviagtion  -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <!-- Start Toggle Navigation For Mobile display -->
+        <div class="navbar-header">             
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">                            
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>                    
+            <a class="navbar-brand" href="{{ url('/') }}">Friends Of Allamano Foundation</a>           
+        </div>
+        <!-- End Toggle Navigation For Mobile display -->
+
+        <!-- Start collecting the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">  
+            <ul class="nav navbar-nav navbar-right custom-menu">
+                <li class="hidden">
+                    <a href="{{ url('/') }}"></a>
+                </li>
+                <li>
+                    <a class="active" href="{{ url('/') }}">Home</a>
+                </li>
+                <li>
+                    <a href="{{ url('about') }}">About Us</a>
+                </li>
+                <li>
+                    <a href="{{ url('shop') }}">Shop</a>
+                </li>
+                <li>
+                    <a href="{{ url('blog') }}">Blog</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="{{ url('contact') }}">Contact</a>
+                </li>
+            </ul>                        
+        </div>
+        <!-- /.navbar-collapse -->             
+    </div>
+    <!-- /.container -->
+</nav>
+<!-- End Header Logo & Naviagtion --> 
