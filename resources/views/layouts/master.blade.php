974bc07c412d2d00aad5105174e1d35fd19b3144
kamaumike <kamaumike2013@gmail.com>
2017-05-16 15:47:22 +0300

    Create master layout file

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">        
+
+        <title>Friends Of Allamano Foundation | @yield('title')</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+
+        <!-- Font Awesome CSS -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/font-awesome/css/font-awesome.min.css') }}">
+
+        <!-- Custom CSS -->        
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/custom.css') }}">                 
+
+    </head>
+    <body data-spy="scroll">
+
+        <!-- Navigation bar -->
+        @include('layouts.nav')
+
+        <!-- Display Content -->
+        @yield('content')              
+
+        <!-- Display footer -->
+        
+
+        <!-- jQuery -->
+        <script src="{{ asset('assets/js/jquery.js') }}"></script>
+        
+        <!-- Bootstrap Core JavaScript -->
+        <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+
+        <script src="{{ asset('assets/js/jqueryscroll.js') }}"></script>
+
+    </body>
+</html>
