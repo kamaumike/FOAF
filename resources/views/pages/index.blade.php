dff1e319b97aa4c25c65a48858ede8287b40ae5b
kamaumike <kamaumike2013@gmail.com>
2017-05-16 15:38:31 +0300

    Create 'services' section for homepage

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -77,5 +77,52 @@
 		</div>
 	</section>
 
-
+	<section id="services">
+		<div class="orangeback">
+			<div class="container">
+				<div class="text-center homeport2"><h2>WHAT WE DO</h2></div>
+				<div class="row">
+					<div class="col-md-12 homeservices1">
+						<div class="col-md-4 portfolio-item">
+							<div class="text-center">
+								<a href="javascript:void(0);">
+								<span class="fa-stack fa-lg">
+								  <i class="fa fa-circle fa-stack-2x"></i>
+								  <i class="fa fa-search fa-stack-1x"></i>
+								</span>
+								</a>
+								<h3><a href="#">Research</a></h3>
+								<p>We research on types of mental and physical disabilities, causes of disabilities, possible treatment &amp; prevention measures, challenges faced by persons with disabilities &amp; solutions to these challenges.</p>
+							</div>
+						</div>
+						<div class="col-md-4 portfolio-item">
+							<div class="text-center">
+								<a href="javascript:void(0);">
+								<span class="fa-stack fa-lg">
+								  <i class="fa fa-circle fa-stack-2x"></i>
+								  <i class="fa fa-bullhorn fa-stack-1x"></i>
+								</span>
+								</a>
+								<h3><a href="#">Disability Awareness</a></h3>
+								<p>We create disability awareness through blogs, monthly newsletters, organizing visits to institutions of persons with disabilities,holding events on persons with disabilities(PWDs) &amp; selling books on disability issues.These channels will "Hatch" informed minds &amp; humane perspectives towards persons with disabilities.</p>
+							</div>
+						</div>
+						<div class="col-md-4 portfolio-item">
+							<div class="text-center">
+								<a href="javascript:void(0);">
+								<span class="fa-stack fa-lg">
+								  <i class="fa fa-circle fa-stack-2x"></i>
+								  <i class="fa fa-wheelchair fa-stack-1x"></i>
+								</span>
+								</a>
+								<h3><a href="#">Support the Disabled</a></h3>
+								<p>Friends Of Allamano Foundation is at the forefront of improving the lives of persons with disabilities(PWDs) through advocating civil and educational rights of PWDs, selling walking aids(wheelchairs,clutches).</p>
+							</div>
+						</div>						
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
 @endsection
