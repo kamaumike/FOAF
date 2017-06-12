29eb235d2e117bb5d13b75f60dad3939c2b44f8e
kamaumike <kamaumike2013@gmail.com>
2017-06-12 17:39:06 +0300

    Create WishlistController

---
+++ app/Http/Controllers/WishlistController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \Cart as Cart;
+
+class WishlistController extends Controller
+{
+    /**
+     * Display the Wishlist page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('shop.wishlist');
+    }
+
+    /**
+     * Add item to Wishlist.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {        
+        $duplicates = Cart::instance('wishlist')->search(function ($cartItem, $rowId) use ($request) {
+            return $cartItem->id === $request->id;
+        });
+
+        if (!$duplicates->isEmpty()) {
+            return redirect('/shop')->withSuccessMessage('Item is already in your wishlist!');
+        }
+
+        Cart::instance('wishlist')->add($request->id, $request->name, 1, $request->price)
+                                  ->associate('App\Product');
+
+        return redirect('/shop')->withSuccessMessage('Item was added to your wishlist!');       
+    }
+
+    /**
+     * Remove an item from Wishlist.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {        
+        Cart::instance('wishlist')->remove($id);
+        return redirect('/wishlist')->withSuccessMessage('Item has been removed!');        
+    }
+}
