57d5e0fc9ab3014b840cb6b0f71d382be1802ad9
kamaumike <kamaumike2013@gmail.com>
2017-07-21 23:06:36 +0300

    Create 'Payment' model

---
+++ app/Payment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'payment_method_id', 'order_id', 'transaction_id', 'currency_code', 'amount_paid', 'payment_status',
+    ];
+
+    // Get payment method associated with a payment
+    public function paymentMethod()
+    {
+
+    	return $this->belongsTo(PaymentMethod::class);
+
+    }    
+}
